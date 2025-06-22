def70b7efba0bb08696ae17a31a7513f42fa100c
RakshitDevra <110564848+RakshitDevra@users.noreply.github.com>
2025-06-23 00:10:30 +0530

    Added updated Resume and Leetcode Questions

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,6 +38,10 @@ Feel free to explore my portfolio to see some of the projects I &apos; ve worked
 </div>
 <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row md:order-3 '> 
 <div className='flex flex-col items-end justify-center xl:items-center '>
+<span className='inline-block text-7xl font-bold  md:text-6xl sm:text-5xl xs:text-4xl '>
+    1000+
+  </span>
+  <h3 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75   xl: text-center md:text-lg sm:text-base xs:text-sm '> DSA Problems solved on Leetcode</h3>
   <span className='inline-block text-7xl font-bold  md:text-6xl sm:text-5xl xs:text-4xl '>
     10+
   </span>
