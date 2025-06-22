1f6b5fe7927779f70573805d46641dd054ab382a
RakshitDevra <110564848+RakshitDevra@users.noreply.github.com>
2025-06-23 00:33:21 +0530

    Added Updated Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
 
           </p>
           <div className='flex items-center self-start mt-2 lg:self-center '>
-            <Link href="/RakshitDevraResume.pdf" target={"_blank"} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-solid  border-transparent border-2 hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base   ' download={true}>Resume <LinkArrow className={"w-6 ml-1 "}/></Link>
+            <Link href="/RakshitDevra_Resume.pdf" target={"_blank"} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-solid  border-transparent border-2 hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base   ' download={true}>Resume <LinkArrow className={"w-6 ml-1 "}/></Link>
             <Link href="mailto:rakshitdevra123@gmail.com?subject=Regarding Employment Opportunity" target={"_blank"} className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base '>Contact</Link>
           </div>
         </div>
